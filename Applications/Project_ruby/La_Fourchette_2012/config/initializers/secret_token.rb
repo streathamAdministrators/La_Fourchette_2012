7f35b79744f67f02bfa9b0a8898d979fc1de87ec
Damien PUIG <puigfb@gmail.com>
2012-05-25 13:44:21 +0100

    mise en place de l'application Rails

---
+++ Applications/Project_ruby/La_Fourchette_2012/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaFourchette2012::Application.config.secret_token = '979e3e4f3fe6ccbff47e345909928754269425f1dddb609295b6137bdd912259baac2d0752e0544059f27aa6443c2596fc6d1e2f35622df7bd27de9503f9116f'
