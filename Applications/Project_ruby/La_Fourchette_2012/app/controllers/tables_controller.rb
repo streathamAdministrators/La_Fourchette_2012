4df965d642012f1b5eab6b0dd51a4efd3c84ca2e
Régis NIOX <regis152@hotmail.com>
2012-06-05 15:31:35 +0100

    Merge table_controller, index, show

--- Applications/Project_ruby/La_Fourchette_2012/app/controllers/tables_controller.rb
+++ Applications/Project_ruby/La_Fourchette_2012/app/controllers/tables_controller.rb
@@ -2,8 +2,7 @@ class TablesController < ApplicationController
   # GET /tables
   # GET /tables.json
   def index
-    @table = ""
-  #@test = params['regis']*/
+    @table = Table.new
   end
 
   # GET /tables/1
@@ -49,46 +48,53 @@ class TablesController < ApplicationController
     end
   end
 
+  # POST /tables
+  # Permet d'initialiser la table en checkant son availability.
   def initTable
-    @table = Table.find_by_numero_table(params['table'])
-    if @table != nil && @table.is_available == true
-      redirect_to :action => 'show', :id => @table.id
-    @table.is_available = 0
-    @table.save
-    else if @table.is_available == 0
-        redirect_to :action => 'index' #, notice: 'Table occupée'
-      else
-        redirect_to :action => 'index' #, notice: 'Table inexistante
-      end
+    # find_by_numero_table ne peut pas fonctionner car pas de champs numero_table dans le model
+    @table = Table.find_by_number_table(params[:number_table])
+    # tu peux aussi l'ecrire @table.nil?
+    if @table != nil && @table.is_available == true 
+      # Penses à utiliser les routes REST prévu par RoR.
+      redirect_to table_path(@table) #:action => 'show', :id => @table.id
+      @table.is_available = 0
+      # Je me demande s'il est pas plus judicieux d'utiliser .update (à voir)
+      @table.save
+    elsif @table.is_available == 0
+      # Route en pluggant à REST
+      redirect_to tables_path #:action => 'index' #, notice: 'Table occupée'
+    else
+      # Route en pluggant à REST
+      redirect_to tables_path #:action => 'index' #, notice: 'Table inexistante
     end
+  end
 
-    # PUT /tables/1
-    # PUT /tables/1.json
-    def update
-      @table = Table.find(params[:id])
+  # PUT /tables/1
+  # PUT /tables/1.json
+  def update
+    @table = Table.find(params[:id])
 
-      respond_to do |format|
-        if @table.update_attributes(params[:table])
-          format.html { redirect_to @table, notice: 'Table was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @table.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
-    # DELETE /tables/1
-    # DELETE /tables/1.json
-    def destroy
-      @table = Table.find(params[:id])
-      @table.destroy
-
-      respond_to do |format|
-        format.html { redirect_to tables_url }
+    respond_to do |format|
+      if @table.update_attributes(params[:table])
+        format.html { redirect_to @table, notice: 'Table was successfully updated.' }
         format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @table.errors, status: :unprocessable_entity }
       end
     end
   end
-  
+
+  # DELETE /tables/1
+  # DELETE /tables/1.json
+  def destroy
+    @table = Table.find(params[:id])
+    @table.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tables_url }
+      format.json { head :no_content }
+    end
+  end
+    
 end
