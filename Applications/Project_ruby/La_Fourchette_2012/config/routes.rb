2ee6e53d2e31b5b8499c37a96a1ef7dc878b0263
Thibault <laurens.thibault@gmail.com>
2012-06-06 23:30:54 +0100

    change_element_state en cours

--- Applications/Project_ruby/La_Fourchette_2012/config/routes.rb
+++ Applications/Project_ruby/La_Fourchette_2012/config/routes.rb
@@ -53,13 +53,16 @@ LaFourchette2012::Application.routes.draw do
         resources :waiters, :kitchen
         
         get '/tables' => 'waiters#table', :as => 'waiter_table'
+        get '/orders/:table_id' => 'waiters#order', :as => 'order'
         
-        get '/orders/:table_id' => 'waiters#index'
         get '/check_orders/:table_id' => 'waiters#check_orders', :as => 'waiter_check_orders'
-        get '/change_order_states/:element_id' => 'waiters#change_order_states', :as => 'waiter_order_states'
+        
+        get '/change_element_states/:element_id' => 'waiters#change_element_states', :as => 'waiter_element_states'
+        
         
         get '/elements' => 'kitchens#index', :as => 'element_index'
         get '/check_elements' => 'kitchens#check_elements', :as => 'kitchen_check_elements'
+        
         match '/kitchens/accept/:id' => 'kitchens#accept', :as => 'kitchen_accept'
         match '/kitchens/cooked/:id' => 'kitchens#cooked', :as => 'kitchen_cooked'
         
